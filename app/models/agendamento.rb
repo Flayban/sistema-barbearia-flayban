4db3e8b428aa6f944810cf08b7b5ecbeb97f6fa6
David Lucas Flayban dos Santos Melo <davidflayban2@hotmail.com>
2022-09-21 12:41:01 -0300

    validates basicos de agendamento

--- app/models/agendamento.rb
+++ app/models/agendamento.rb
@@ -1,4 +1,8 @@
 class Agendamento < ApplicationRecord
   belongs_to :barbeiro
   belongs_to :cliente
+
+  validates :horario, presence: true
+  validates :barbeiro_id, presence: true
+  validates :cliente_id, presence: true
 end
