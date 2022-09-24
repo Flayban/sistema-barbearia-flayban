b05b3f5ac90f59d075d700dd3232ff70becddb0b
jvictorcordeiro <jvictorcor@hotmail.com>
2022-09-24 19:43:41 -0300

    adicionando validations de cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,16 @@
+require 'phonelib'
 class Cliente < ApplicationRecord
+  ##has_many agendamentos, dependent: :destroy
+  ##has_many :barbeiros, through: :agendamentos
+
+  validates :nome, presence: true, length: {in:3..35}, format: {with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/ }
+  validates :telefone, phone: { possible: true, allow_blank: false , types: [:voip, :mobile], }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
+  validate :nascimento_valido
+
+  def nascimento_valido
+    if data_nascimento.present? && data_nascimento > Date.today
+      errors.add(:data_nascimento, ': Data de nascimento invalida')
+    end
+  end
 end
