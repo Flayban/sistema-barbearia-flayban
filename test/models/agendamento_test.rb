b014f16c713acca1cc15eeac924252ac5f328d5c
Caio <caio.vsg@hotmail.com>
2022-09-25 14:58:54 -0300

    adicao dos testes de model da classe de agendamento

--- test/models/agendamento_test.rb
+++ test/models/agendamento_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class AgendamentoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "criar agendamento valido" do
+    cliente = Cliente.new(nome:"Jucas", telefone:"40028922", email: "email@email.com", data_nascimento: "09/02/1997")
+    assert cliente.save
+
+    barbeiro = Barbeiro.new(nome: "Indigo", cpf: "55555555555", data_nascimento: "02/07/1990", telefone: "34567890",
+                            email: "barbeiro@email.com")
+    assert barbeiro.save
+
+    agendamento = Agendamento.new(inicioAtendimento: "14:00", cliente_id: cliente.id, barbeiro_id: barbeiro.id)
+
+    assert agendamento.save
+  end
+
+  test "criar agendamento com horario invalido" do
+    cliente = Cliente.new(nome:"Jucas", telefone:"40028922", email: "email@email.com", data_nascimento: "09/02/1997")
+    assert cliente.save
+
+    barbeiro = Barbeiro.new(nome: "Indigo", cpf: "55555555555", data_nascimento: "02/07/1990", telefone: "34567890",
+                            email: "barbeiro@email.com")
+    assert barbeiro.save
+
+    agendamento = Agendamento.new(inicioAtendimento: "12:00", cliente_id: cliente.id, barbeiro_id: barbeiro.id)
+
+    assert agendamento.save
+  end
 end
