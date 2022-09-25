9383c4e388bd05ee13a2da9702b35ef8412fb141
David Lucas Flayban dos Santos Melo <davidflayban2@hotmail.com>
2022-09-24 21:05:54 -0300

    atualização no model cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,15 @@
 class Cliente < ApplicationRecord
-  has_many :agendamentos
+  has_many agendamentos, dependent: :destroy
+  ##has_many :barbeiros, through: :agendamentos
+
+  validates :nome, presence: true, length: {in:3..35}, format: {with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/ }
+  validates :telefone, phone: { possible: true, allow_blank: false , types: [:voip, :mobile], }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
+  validate :nascimento_valido
+
+  def nascimento_valido
+    if data_nascimento.present? && data_nascimento > Time.zone.today
+      errors.add(:data_nascimento, ': Data de nascimento invalida')
+    end
+  end
 end
