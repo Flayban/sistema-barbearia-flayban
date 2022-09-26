44d1fbdf4232cbed77f96e7b088ae603fd68c7bb
Caio <caio.vsg@hotmail.com>
2022-09-26 17:29:42 -0300

    Correcao dos testes de model de agendamento

--- test/models/agendamento_test.rb
+++ test/models/agendamento_test.rb
@@ -14,7 +14,7 @@ class AgendamentoTest < ActiveSupport::TestCase
     assert agendamento.save
   end
 
-  test "criar agendamento com horario invalido" do
+  test "criar agendamento no horario de almoco" do
     cliente = Cliente.new(nome:"Jucas", telefone:"08740028922", email: "email@email.com", data_nascimento: "09/02/1997")
     assert cliente.save
 
@@ -23,6 +23,19 @@ class AgendamentoTest < ActiveSupport::TestCase
     assert barbeiro.save
 
     agendamento = Agendamento.new(inicioAtendimento: "12:00", cliente_id: cliente.id, barbeiro_id: barbeiro.id)
+
+    assert_not agendamento.save
+  end
+
+  test "criar agendamento apos o fechamento" do
+    cliente = Cliente.new(nome:"Jucas", telefone:"08740028922", email: "email@email.com", data_nascimento: "09/02/1997")
+    assert cliente.save
+
+    barbeiro = Barbeiro.new(nome: "Indigo", cpf: "27071571040", data_nascimento: "02/07/1990", telefone: "08734568901",
+                            email: "barbeiro@email.com")
+    assert barbeiro.save
+
+    agendamento = Agendamento.new(inicioAtendimento: "23:00", cliente_id: cliente.id, barbeiro_id: barbeiro.id)
 
     assert_not agendamento.save
   end
