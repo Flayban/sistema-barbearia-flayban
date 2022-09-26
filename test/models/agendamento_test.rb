fb51a75a0e00d206209cf63fad69c8a5db97ba7b
Caio <caio.vsg@hotmail.com>
2022-09-26 17:25:58 -0300

    Correcao dos testes de model de agendamento

--- test/models/agendamento_test.rb
+++ test/models/agendamento_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class AgendamentoTest < ActiveSupport::TestCase
   test "criar agendamento valido" do
-    cliente = Cliente.new(nome:"Jucas", telefone:"40028922", email: "email@email.com", data_nascimento: "09/02/1997")
+    cliente = Cliente.new(nome:"Jucas", telefone:"08740028922", email: "email@email.com", data_nascimento: "09/02/1997")
     assert cliente.save
 
-    barbeiro = Barbeiro.new(nome: "Indigo", cpf: "55555555555", data_nascimento: "02/07/1990", telefone: "34567890",
+    barbeiro = Barbeiro.new(nome: "Indigo", cpf: "27071571040", data_nascimento: "02/07/1990", telefone: "08734568901",
                             email: "barbeiro@email.com")
     assert barbeiro.save
 
@@ -15,15 +15,15 @@ class AgendamentoTest < ActiveSupport::TestCase
   end
 
   test "criar agendamento com horario invalido" do
-    cliente = Cliente.new(nome:"Jucas", telefone:"40028922", email: "email@email.com", data_nascimento: "09/02/1997")
+    cliente = Cliente.new(nome:"Jucas", telefone:"08740028922", email: "email@email.com", data_nascimento: "09/02/1997")
     assert cliente.save
 
-    barbeiro = Barbeiro.new(nome: "Indigo", cpf: "55555555555", data_nascimento: "02/07/1990", telefone: "34567890",
+    barbeiro = Barbeiro.new(nome: "Indigo", cpf: "27071571040", data_nascimento: "02/07/1990", telefone: "08734568901",
                             email: "barbeiro@email.com")
     assert barbeiro.save
 
     agendamento = Agendamento.new(inicioAtendimento: "12:00", cliente_id: cliente.id, barbeiro_id: barbeiro.id)
 
-    assert agendamento.save
+    assert_not agendamento.save
   end
 end
