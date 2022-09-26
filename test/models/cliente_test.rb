95f428f9b2d6141764a77b90376675ada178dcb0
jvictorcordeiro <jvictorcor@hotmail.com>
2022-09-26 20:33:16 -0300

    adicionando testes de model para cliente

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ClienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Criando cliente com nascimento inválido' do
+    cliente = Cliente.new nome: 'João Cordeiro', telefone: '87988625344',
+                          email: 'teste@test.com', data_nascimento: Time.zone.tomorrow
+    assert_not cliente.save
+  end
+  test 'Criando cliente com telefone invalido' do
+    cliente = Cliente.new nome: 'João Victor', telefone: '40028922',
+                          email: 'teste@teste.com', data_nascimento: '29/05/2001'
+    assert_not cliente.save
+  end
+  test 'Criando cliente com dados válidos' do
+    cliente = Cliente.new nome: 'João Victor', telefone: '87988625344',
+                          email: 'teste@teste.com', data_nascimento: '29/06/2000'
+    assert cliente.save
+  end
 end
