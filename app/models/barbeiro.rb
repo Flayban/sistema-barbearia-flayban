9295ab66a67b0eccb2c8fac1180d2941421c1b0e
Eduarda Interaminense <eduardainteraminense@gmail.com>
2022-09-27 18:36:26 -0300

    Relacionamento barbeiro x cliente

--- app/models/barbeiro.rb
+++ app/models/barbeiro.rb
@@ -1,9 +1,9 @@
 class Barbeiro < ApplicationRecord
-  has_many :agendamentos
+  has_many :agendamentos, :dependent => :destroy
+  has_many :clientes, :through => :agendamentos
 
   validates :nome, presence: true, numericality: false, length: {in: 5..40},
             format: { with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/}
-
   validate :cpf_valido
   validate :data_nascimento_valida
   validates :telefone, presence: true, numericality: true, length: { is: 11 }
