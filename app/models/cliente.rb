83731c22f573f3d3abfb463b62b9aa38413d6dfd
jvictorcordeiro <jvictorcor@hotmail.com>
2022-09-26 22:47:40 -0300

    adicionando validates de unicidade ao model de clientes

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -5,8 +5,8 @@ class Cliente < ApplicationRecord
   ##has_many :barbeiros, through: :agendamentos
 
   validates :nome, presence: true, length: {in:3..35}, format: {with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/ }
-  validates :telefone, phone: { possible: true, allow_blank: false , types: [:voip, :mobile], }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}
+  validates :telefone, phone: { possible: true, allow_blank: false , types: [:voip, :mobile] }, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP}, uniqueness: true
   validate :nascimento_valido
 
   def nascimento_valido
