ab60e856659714e59f7b285cab67eb6a0ddd0d4a
David Lucas Flayban dos Santos Melo <davidflayban2@hotmail.com>
2022-09-28 11:31:10 -0300

    atualização na relaçao de cliente e barbeiro

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,7 +2,7 @@ require 'phonelib'
 class Cliente < ApplicationRecord
 
   has_many :agendamentos, :dependent => :destroy
-  ##has_many :barbeiros, through: :agendamentos
+  has_many :barbeiros, through: :agendamentos
 
   validates :nome, presence: true, length: {in:3..35}, format: {with: /\A[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+\z/ }
   validates :telefone, phone: { possible: true, allow_blank: false , types: [:voip, :mobile] }, uniqueness: true
