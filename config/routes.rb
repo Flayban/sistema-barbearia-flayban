718f8fd092cec83ed8ac61e40f5c2c9dab8d7431
David Lucas Flayban dos Santos Melo <davidflayban2@hotmail.com>
2022-09-28 10:43:48 -0300

    atualização nas rotas de Welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-
+  get 'welcome/index'
+  get 'welcome/new'
+  get 'welcome/creat'
   resources :barbeiros
   resources :clientes do
     resources :agendamentos
   end
+  resources :welcome
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
