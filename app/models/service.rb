4c76a616a1c99f21cfbdcf1bf6e3948a2b24e9cd
David Lucas Flayban dos Santos Melo <davidflayban2@hotmail.com>
2022-10-06 17:55:01 -0300

    Adicao dos validates de Service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,6 @@
 class Service < ApplicationRecord
   has_many :agendamentos
+  validates :nome, presence: true, length: {in:3..35}
+  validates :valor, presence: true, length: {in:3..10}
+  validates :tempo, presence: true, length: {in:1..5}
 end
