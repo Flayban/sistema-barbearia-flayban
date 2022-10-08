20698b83db46a20adf2f2d08cdfab4e86a56d7f2
David Lucas Flayban dos Santos Melo <davidflayban2@hotmail.com>
2022-10-08 08:47:44 -0300

    Criacao dos testes de Model da CRUD Service

--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ServiceTest < ActiveSupport::TestCase
+
   # test "the truth" do
   #   assert true
   # end
+  test "Criando serviço com valor invalidos" do
+    service = Service.new nome:"Corte de cabelo e barba", valor:"25", tempo: "30"
+    assert_not service.save
+  end
+  test "Criando serviço com nome invalidos" do
+    service = Service.new nome:"Corte de cabelo com corte de barba e massagem facial", valor:"100,00", tempo: "30"
+    assert_not service.save
+  end
+  test "Criando serviço com dados validos" do
+    service = Service.new nome:"Corte apenas de cabelo", valor:"10,00", tempo: "30"
+    assert service.save
+  end
 end
